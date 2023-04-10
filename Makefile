eb931cdc5f9383e3bcbb503e8de456ba664cdb50
Sina Siadat <siadat@gmail.com>
2023-04-10 14:45:16 +0000

    first implementation

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+	DEFAULT_OPENAI_API_KEY=$(shell cat openai_key) \
+	   poetry run uvicorn main:app --host 127.0.0.1 --port 8000 --reload --workers 4 --limit-concurrency 4
--- main.py
+++ main.py
@@ -1,17 +1,108 @@
-from fastapi import FastAPI
+# vim: noexpandtab tabstop=4 softtabstop=4 shiftwidth=4 autoindent textwidth=120
+import os
+import openai
+import yaml
+import json
+import random
+import time
+import logging
+import uuid
+from textwrap import dedent
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from typing import Optional
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+api_key = os.getenv("DEFAULT_OPENAI_API_KEY")
 app = FastAPI()
+random_number_generator = random.Random()
 
-class Data(BaseModel):
-    field1: str
-    field2: int
+with open("things.yaml") as f:
+	things = yaml.safe_load(f.read())
 
-@app.post("/process_data")
-async def process_data(data: Data):
-    response = {
-        "received_field1": data.field1,
-        "received_field2": data.field2,
-        "result": data.field1 * data.field2
-    }
-    return response
+def get_todays_answer() -> str:
+	epoch_days = int(int(time.time()) / 3600 / 24)
+	random_number_generator.seed(epoch_days)
+	idx = random_number_generator.randint(0, len(things.get("answers", []))-1)
+	answer = things.get("answers", [])[idx].lower()
+	return answer
+
+def generate_prompt(*, answer: str, guess: str) -> str:
+	guess = guess.strip()[:50]
+	guess = guess.replace("\n", " ")
+	guess = guess.replace("\r", " ")
+	guess = guess.lower()
+
+	# "it depends"
+	# "not applicable"
+	return dedent(f"""
+		Let's play a Twenty-Questions quiz game. Here are the rules:
+
+		- The Player asks questions, to guess the answer.
+		- The Host doesn't mention the answer until the Player has correctly guessed it.
+		- The Host only responds with "yes", "no", "maybe", "probably not", "related", or "correct, well done!".
+
+		The answer for this round is {repr(answer)}. Let's start.
+
+		Player: {guess}
+		Answerer: 
+	""")
+
+class Input(BaseModel):
+	text: str
+	openai_key: Optional[str]
+
+def get_session_id(request: Request) -> str:
+	session_id = request.cookies.get("session_id")
+	if not session_id:
+		session_id = str(uuid.uuid4())
+	return session_id
+
+@app.post("/guess")
+async def process_data(request: Request, data: Input):
+	session_id = get_session_id(request)
+	wanted_answer = get_todays_answer()
+	guess = data.text
+	prompt = generate_prompt(answer=wanted_answer, guess=guess)
+
+	llm_response = openai.Completion.create(
+		model="text-davinci-003",
+		prompt=prompt,
+		temperature=1,
+		max_tokens=100,
+		top_p=1,
+		frequency_penalty=0.0,
+		presence_penalty=0.0,
+		stop=["\n"],
+		api_key=api_key,
+	)
+
+	response_text = ""
+
+	try:
+		response_text = llm_response.choices[0].text
+	except Exception as e:
+		response_text = "Oops, something went wrong, please take a deep breath, and try again."
+		logger.error(json.dumps({
+			"session_id": session_id,
+			"error": e,
+		}))
+	finally:
+		logger.info(json.dumps({
+			"session_id": session_id,
+			"answer": wanted_answer,
+			"guess": guess,
+			"response_text": response_text,
+			"prompt": prompt,
+			"response": llm_response,
+		}))
+
+	response = JSONResponse(content={
+		"response": response_text,
+	})
+	response.set_cookie(key="session_id", value=session_id)
+
+	return response
